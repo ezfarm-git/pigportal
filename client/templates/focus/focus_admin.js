3ab88699fe619619abacac2d9b68d9521cb02239
wooyol <neolaocoon@naver.com>
2016-06-05 22:39:43 +0900

    focus autoform complete

--- client/templates/focus/focus_admin.js
+++ client/templates/focus/focus_admin.js
@@ -1,17 +1,14 @@
 Template.focus_admin.onCreated(function() {
-    // this.selectedDoc = new ReactiveVar("");
-    // this.selectedImage = new ReactiveVar("");
-    // this.formType = new ReactiveVar("insert");
-    Session.set("ImageId", "");
-    Session.set("PostId", "");
+    this.selectedDoc = new ReactiveVar("");
+    this.selectedImage = new ReactiveVar("");
+    this.formType = new ReactiveVar("insert");
 });
 
 Template.focus_admin.onDestroyed(function() {
     document.getElementById('Form').reset();
-    // this.selectedDoc.set(null);
-    // this.selectedImage.set(null);
-    Session.set("ImageId", null);
-    Session.set("PostId", null);
+    this.selectedDoc.set(null);
+    this.selectedImage.set(null);
+    this.formType.set(null);
 });
 
 Template.focus_admin.helpers({
@@ -24,99 +21,46 @@ Template.focus_admin.helpers({
             showColumnToggles: true,
             multiColumnSort: false,
             showNavigationRowsPerPage: false,
-            // enableRegex: true,
             fields: ['date', 'title', 'summary', 'category']
-            // filters: ['date', 'category']
         };
     },
     selectedDoc: function() {
-        // return Focus.findOne(Template.instance().selectedDoc.get());
-        return Focus.findOne(Session.get("PostId"));
+        return Focus.findOne(Template.instance().selectedDoc.get());
     },
     selectedImage: function() {
-        // return Template.instance().selectedImage.get();
-        return Session.get("ImageId");
+        return Template.instance().selectedImage.get();
     },
     formType: function() {
-        // if (Template.instance().selectedDoc.get()) {
-        if (Session.get("PostId")) {
-            return "update";
-        } else {
-            return "insert";
-        }
+        return Template.instance().formType.get();
     }
 });
 
 Template.focus_admin.events({
     'click .reactive-table tbody tr': function(evt, tmpl) {
-        evt.preventDefault();
         $('#postId').val(this._id);
         $('#imageId').val(this.image);
-        // $('#date').val(this.date);
-        // $('#title').val(this.title);
-        // $('#summary').val(this.summary);
-        // $('.note-editable').html(this.content);
-        // $('#category').val(this.category);
-        // $('.addPost').text('Update Post').removeClass('addPost').addClass('updatePost');
-        // tmpl.selectedDoc.set(this._id);
-        // tmpl.selectedImage.set(this.image);
-        // tmpl.formType.set("update");
-        Session.set("PostId", this._id);
-        Session.set("ImageId", this.image);
-        $('.submit-btn').text('Edit').removeClass('submit-btn').addClass('edit-btn');
+        evt.preventDefault();
+        tmpl.selectedDoc.set(this._id);
+        tmpl.selectedImage.set(this.image);
+        tmpl.formType.set("update");
+        // $('.submit-btn').text('Edit').removeClass('submit-btn').addClass('edit-btn');
     },
-    // 'click .updatePost': function(evt, tmpl) {
-    //     evt.preventDefault();
-    //     var id = tmpl.find('#postId').value;
-    //     var image = tmpl.find('#imageId').value;
-    //     var date = tmpl.find('#date').value;
-    //     var title = tmpl.find('#title').value;
-    //     var summary = tmpl.find('#summary').value;
-    //     var content = $('.note-editable').html();
-    //     var category = tmpl.find('#category').value;
-    //     Meteor.call('Focus.update', id, date, title, summary, content, image, category);
-    //     // var temp_imageId = Session.get("temp_imageId");
-    //     // Meteor.call('Focus.imageUpdate', id, temp_imageId);
-    //     $('.updatePost').text('Add Post').removeClass('updatePost').addClass('addPost');
-    //     document.getElementById('Form').reset();
-    //     Session.set("ImageId", "");
-    // },
-    // 'click .edit-btn': function() {
-        // $('.edit-btn').text('Add').removeClass('edit-btn').addClass('submit-btn');
-    // },
     'click .remove-btn': function(evt, tmpl) {
         evt.preventDefault();
-        var postId = tmpl.find('#postId').value;
-        var imageId = tmpl.find('#imageId').value;
+        var postId = tmpl.selectedDoc.get();
+        var imageId = tmpl.selectedImage.get();
         Meteor.call('Focus.remove', postId);
         Meteor.call('Images.remove', imageId);
         document.getElementById('Form').reset();
-        // tmpl.selectedDoc.set(null);
-        // tmpl.selectedImage.set(null);
-        // tmpl.formType.set("insert");
-        Session.set("PostId", null);
-        Session.set("ImageId", null);
+        tmpl.selectedDoc.set("");
+        tmpl.selectedImage.set("");
+        tmpl.formType.set("insert");
     },
     'click .reset-btn': function(evt, tmpl) {
         evt.preventDefault();
         document.getElementById('Form').reset();
-        // tmpl.selectedDoc.set(null);
-        // tmpl.selectedImage.set(null);
-        // tmpl.formType.set("insert");
-        Session.set("PostId", null);
-        Session.set("ImageId", null);
-    },
-    'click .deleteImage': function(evt, tmpl) {
-        evt.preventDefault();
-        var postId = tmpl.find('#postId').value;
-        var imageId = tmpl.find('#imageId').value;
-        Meteor.call('Focus.remove', postId);
-        Meteor.call('Images.remove', imageId);
-        document.getElementById('Form').reset();
-        // tmpl.selectedDoc.set(null);
-        // tmpl.selectedImage.set(null);
-        // tmpl.formType.set("insert");
-        Session.set("PostId", null);
-        Session.set("ImageId", null);
+        tmpl.selectedDoc.set("");
+        tmpl.selectedImage.set("");
+        tmpl.formType.set("insert");
     }
 });
