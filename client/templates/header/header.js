347b18b5ce082b4b290de081b9a7331ae7a9d637
wooyol <neolaocoon@naver.com>
2016-06-17 14:14:57 +0900

    link target _top

--- client/templates/header/header.js
+++ client/templates/header/header.js
@@ -2,4 +2,12 @@ Template.header.onRendered(function() {
     $('.menu').on('click', function() {
         $('.navbar-collapse').collapse('hide');
     });
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() < 400) {
+            $('.navbar').fadeIn('slow');
+        } else {
+            $('.navbar').fadeOut('slow');
+        }
+    });
 });
