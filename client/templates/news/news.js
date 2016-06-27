e4a5cc39cad196bd3014f3482b28e8e9375c6874
wooyol <neolaocoon@naver.com>
2016-06-27 14:07:44 +0900

    ga js code test

--- client/templates/news/news.js
+++ client/templates/news/news.js
@@ -5,6 +5,14 @@ Template.news.onCreated(function() {
         var currentPage = parseInt(Router.current().params.page) || 1;
         var skipCount = (currentPage - 1) * 15; // 15 records per page
         self.subscribe('newsList', category, skipCount);
+
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-79916323-1', 'auto');
+        ga('send', 'pageview');
     });
 });
 
