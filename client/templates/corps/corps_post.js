5eeca4b41369a0fa0b035ed7f91983316233c798
wooyol <neolaocoon@naver.com>
2016-06-28 10:55:21 +0900

    share edit

--- client/templates/corps/corps_post.js
+++ client/templates/corps/corps_post.js
@@ -9,12 +9,12 @@ Template.corps_post.helpers({
 
 Template.corps_post.events({
     'click .nv-share': function() {
-        window.open('https://band.us/plugin/share?body='+encodeURIComponent("Hello")+'&route='+encodeURIComponent(document.URL), 'bandsharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;
+        window.open('https://band.us/plugin/share?body='+encodeURIComponent($('#content').val())+'&route='+encodeURIComponent(document.URL), 'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
     },
     'click .kk-share': function() {
-        window.open('https://story.kakao.com/share?url='+encodeURIComponent(document.URL), 'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;
+        window.open('https://story.kakao.com/share?url='+encodeURIComponent(document.URL), 'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
     },
     'click .fb-share': function() {
-        window.open('https://www.facebook.com/dialog/share?app_id=150240602048486&display=popup&href='+encodeURIComponent(document.URL)+'&redirect_uri=http://pig-portal.herokuapp.com', 'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;
+        window.open('https://www.facebook.com/dialog/share?app_id=150240602048486&display=popup&href='+encodeURIComponent(document.URL)+'&redirect_uri=http://pig-portal.herokuapp.com', 'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
     }
 })
