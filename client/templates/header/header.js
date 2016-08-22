48ec8c963400d260500e8d8e6427d074c0eee31d
wooyol <neolaocoon@naver.com>
2016-08-22 16:53:27 +0900

    apply materialize

--- client/templates/header/header.js
+++ client/templates/header/header.js
@@ -1,13 +1,24 @@
-Template.header.onRendered(function() {
-    $('.menu').on('click', function() {
-        $('.navbar-collapse').collapse('hide');
-    });
+Template.header.onRendered(function () {
 
-    $(window).on('scroll', function() {
-        if ($(this).scrollTop() < 400) {
-            $('.navbar').fadeIn('slow');
-        } else {
-            $('.navbar').fadeOut('slow');
-        }
-    });
+  $(".dropdown-button").dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    hover: true,
+    belowOrigin: true,
+    alignment: 'right'
+  });
+
+  $(".button-collapse").sideNav();
+
+  // $('.menu').on('click', function () {
+  //   $('.navbar-collapse').collapse('hide');
+  // });
+  //
+  // $(window).on('scroll', function () {
+  //   if ($(this).scrollTop() < 400) {
+  //     $('.navbar').fadeIn('slow');
+  //   } else {
+  //     $('.navbar').fadeOut('slow');
+  //   }
+  // });
 });
