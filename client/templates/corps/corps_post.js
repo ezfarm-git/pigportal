60eca36a01d5f2593fb9798c5bac3563ef3a6248
wooyol <neolaocoon@naver.com>
2016-08-25 14:44:52 +0900

    meta tags test

--- client/templates/corps/corps_post.js
+++ client/templates/corps/corps_post.js
@@ -1,31 +1,26 @@
 Template.corps_post.helpers({
-    post: function() {
-        var postId = this.postId;
-        return Corps.find({
-            _id: postId
-        }).fetch()[0];
-    }
+  post: function () {
+    var postId = this.postId;
+    return Corps.find({
+      _id: postId
+    }).fetch()[0];
+  }
 });
 
 Template.corps_post.events({
-    'click .nv-share': function() {
-        window.open('http://band.us/plugin/share?body=' + $('h1').text() + '&route=' + encodeURIComponent(document.URL),
-                    'band', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
-        return false;
-    },
-    'click .kk-share': function() {
-        window.open('https://story.kakao.com/share?url=' + encodeURIComponent(document.URL),
-                    'kakaostory', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-        return false;
-    },
-    'click .fb-share': function() {
-        window.open('https://www.facebook.com/sharer.php?t=' + $('h1').text() + '&u=' + encodeURIComponent(document.URL),
-                    'facebook', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-        return false;
-    }
-    // 'click .fb-share': function() {
-    //     window.open('https://www.facebook.com/dialog/share?app_id=150240602048486&display=popup&href=' + encodeURIComponent(document.URL) + '&redirect_uri=http://pig-portal.herokuapp.com',
-    //                 'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-    //     return false;
-    // }
+  'click .nv-share': function () {
+    window.open('http://band.us/plugin/share',
+      'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
+    return false;
+  },
+  'click .kk-share': function () {
+    window.open('https://story.kakao.com/s/share?url=' + encodeURIComponent(document.URL),
+      'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=540');
+    return false;
+  },
+  'click .fb-share': function () {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL),
+      'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
+    return false;
+  }
 })
--- client/templates/focus/focus_post.js
+++ client/templates/focus/focus_post.js
@@ -1,24 +1,26 @@
 Template.focus_post.helpers({
-    post: function() {
-        var postId = this.postId;
-        return Focus.find({
-            _id: postId
-        }).fetch()[0];
-    }
+  post: function () {
+    var postId = this.postId;
+    return Focus.find({
+      _id: postId
+    }).fetch()[0];
+  }
 });
 
 Template.focus_post.events({
-    'click .nv-share': function() {
-        window.open('http://band.us/plugin/share?body=' + $('h1').text() + '&route=' + encodeURIComponent(document.URL), 'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
-        return false;
-    },
-    'click .kk-share': function() {
-        window.open('https://story.kakao.com/share?url=' + encodeURIComponent(document.URL), 'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-        return false;
-    },
-    'click .fb-share': function() {
-        window.open('https://facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL),
-            'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-        return false;
-    }
+  'click .nv-share': function () {
+    window.open('http://band.us/plugin/share',
+      'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
+    return false;
+  },
+  'click .kk-share': function () {
+    window.open('https://story.kakao.com/s/share?url=' + encodeURIComponent(document.URL),
+      'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=540');
+    return false;
+  },
+  'click .fb-share': function () {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL),
+      'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
+    return false;
+  }
 })
