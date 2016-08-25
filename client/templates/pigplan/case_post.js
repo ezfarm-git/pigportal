f242b5a8ff1fa72288caa867417e2686e4287258
wooyol <neolaocoon@naver.com>
2016-08-25 10:38:56 +0900

    meta tags test

--- client/templates/pigplan/case_post.js
+++ client/templates/pigplan/case_post.js
@@ -1,8 +1,8 @@
-Template.case_post.onRendered(function() {
+Template.case_post.onRendered(function () {
 
   var postId = this.data.postId;
 
-  Meteor.call('get.Case', postId, function(error, result) {
+  Meteor.call('get.Case', postId, function (error, result) {
     if (error) {
       console.log(error);
     } else {
@@ -12,7 +12,7 @@ Template.case_post.onRendered(function() {
 
   var imageId = Session.get('case').image;
 
-  Meteor.call('get.Image', imageId, function(error, result) {
+  Meteor.call('get.Image', imageId, function (error, result) {
     if (error) {
       console.log(error);
     } else {
@@ -22,59 +22,69 @@ Template.case_post.onRendered(function() {
 
   var imageFile = Session.get('image').original.name;
 
-  DocHead.addMeta(
-    {name: "title", content: Session.get('case').title}
-  );
-  DocHead.addMeta(
-    {name: "description", content: Session.get('case').summary}
-  );
-  DocHead.addMeta(
-    {name: "keywords", content: ['양돈', '축산', '돼지', 'pig', 'swine']}
-  );
-  DocHead.addMeta(
-    {name: "og:site_name", content: "Pig Portal"}
-  );
-  DocHead.addMeta(
-    {name: "og:type", content: "article"}
-  );
-  DocHead.addMeta(
-    {name: "og:url", content: window.location.href}
-  );
-  DocHead.addMeta(
-    {name: "og:title", content: Session.get('case').title}
-  );
-  DocHead.addMeta(
-    {name: "og:description", content: Session.get('case').summary}
-  );
-  DocHead.addMeta(
-    {name: "og:image", content: "http://210.92.91.212:3000/cfs/files/Images/" + imageId + "/" + "imageFile"}
-  );
+  // DocHead.addMeta(
+  //   {name: "title", content: Session.get('case').title}
+  // );
+  DocHead.addMeta({
+    name: "description",
+    content: Session.get('case').summary
+  });
+  DocHead.addMeta({
+    name: "keywords",
+    content: ['양돈', '축산', '돼지', 'pig', 'swine']
+  });
+  DocHead.addMeta({
+    name: "og:site_name",
+    content: "Pig Portal"
+  });
+  DocHead.addMeta({
+    name: "og:type",
+    content: "article"
+  });
+  DocHead.addMeta({
+    name: "og:url",
+    content: window.location.href
+  });
+  DocHead.addMeta({
+    name: "og:title",
+    content: Session.get('case').title
+  });
+  DocHead.addMeta({
+    name: "og:description",
+    content: Session.get('case').summary
+  });
+  DocHead.addMeta({
+    name: "og:image",
+    content: "http://210.92.91.212:3000/cfs/files/Images/" + imageId + "/" + imageFile
+  });
+
+  $("meta[property='og:type']").attr('content', Session.get('case').title);
 
 });
 
 Template.case_post.helpers({
-    post: function() {
-        var postId = this.postId;
-        return Case.find({
-            _id: postId
-        }).fetch()[0];
-    }
+  post: function () {
+    var postId = this.postId;
+    return Case.find({
+      _id: postId
+    }).fetch()[0];
+  }
 });
 
 Template.case_post.events({
-    'click .nv-share': function() {
-        window.open('http://band.us/plugin/share',
-                    'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
-        return false;
-    },
-    'click .kk-share': function() {
-        window.open('https://story.kakao.com/s/share?url=' + encodeURIComponent(document.URL),
-                    'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-        return false;
-    },
-    'click .fb-share': function() {
-        window.open('https://www.facebook.com/sharer/sharer.php?app_id=150240602048486&display=popup&u=' + encodeURIComponent(document.URL),
-                    'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-        return false;
-    }
+  'click .nv-share': function () {
+    window.open('http://band.us/plugin/share',
+      'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
+    return false;
+  },
+  'click .kk-share': function () {
+    window.open('https://story.kakao.com/s/share?url=' + encodeURIComponent(document.URL),
+      'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=540');
+    return false;
+  },
+  'click .fb-share': function () {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL),
+      'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
+    return false;
+  }
 })
--- client/templates/test/test2.js
+++ client/templates/test/test2.js
@@ -1,30 +1,7 @@
 Template.test2.onRendered(function() {
-    document.getElementById("datepicker1").defaultValue = moment(new Date()).format('YYYY-MM-DD');
-    document.getElementById("datepicker2").defaultValue = moment(new Date()).format('YYYY-MM-DD');
 
-    function getData() {
-        var start = moment($('#datepicker1').val()).format('YYYYMMDD');
-        var end = moment($('#datepicker2').val()).format('YYYYMMDD');
-        var sex = $('input[name="optradio"]:checked').val();
-        Meteor.call('test2.get', start, end, sex, function(error, result) {
-            if(error) {
-                console.log(error);
-            } else {
-                console.log(start, end, sex);
-                Session.setPersistent('test', result);
-            }
-        });
-    }
-
-    getData();
-
-    $('.container').change(function() {
-        getData();
-    });
 });
 
 Template.test2.helpers({
-    test: function() {
-        return Session.get('test');
-    }
+
 });
