46e29dd9c91c53541b7b56f00a699fcf2f2d4f01
wooyol <neolaocoon@naver.com>
2016-08-26 14:03:58 +0900

    share update

--- client/templates/sharebox/sharebox.js
+++ client/templates/sharebox/sharebox.js
@@ -1,30 +1,34 @@
 Template.sharebox.onRendered(function () {
-
   //<![CDATA[
   // 사용할 앱의 JavaScript 키를 설정해 주세요.
-  Kakao.init('e3aa1678788d2f6b66b63a1384d6b9c7');
+  // Kakao.init('e3aa1678788d2f6b66b63a1384d6b9c7');
   // 스토리 공유 버튼을 생성합니다.
   Kakao.Story.createShareButton({
     container: '#kakaostory-share-button',
     url: document.URL,
+    text: $('#summary').text()
   });
   //]]>
 });
 
 Template.sharebox.events({
   'click .nv-share': function () {
-    window.open('http://band.us/plugin/share?body=' + encodeURIComponent($('#summary').text()) + '&route=' + encodeURIComponent(document.URL),
-      'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
+    window.open('http://band.us/plugin/share?body=' + encodeURIComponent($('#summary').text()) +
+                encodeURIComponent("\n") + encodeURIComponent(document.URL) +
+                '&route=' + encodeURIComponent(document.URL),
+                'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
     return false;
   },
   'click .kk-share': function () {
-    window.open('https://story.kakao.com/s/share?url=' + encodeURIComponent(document.URL),
-      'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=540');
+    window.open('https://story.kakao.com/s/share?url=' +
+                encodeURIComponent(document.URL),
+                'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=540');
     return false;
   },
   'click .fb-share': function () {
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL),
-      'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' +
+                encodeURIComponent(document.URL),
+                'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
     return false;
   }
 })
