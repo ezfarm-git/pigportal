6c75d8955d9f9a51d0480ec2057e4ebf4be75e35
wooyol <neolaocoon@naver.com>
2016-08-26 11:34:20 +0900

    share retry

--- client/templates/pigplan/case_post.js
+++ client/templates/pigplan/case_post.js
@@ -70,19 +70,5 @@ Template.case_post.helpers({
 });
 
 Template.case_post.events({
-  // 'click .nv-share': function () {
-  //   window.open('http://band.us/plugin/share?body=' + encodeURIComponent(Session.get('post').summary) + '&route=' + encodeURIComponent(document.URL),
-  //     'bandsharedialog', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=540,width=410');
-  //   return false;
-  // },
-  // 'click .kk-share': function () {
-  //   window.open('https://story.kakao.com/s/share?url=' + encodeURIComponent(document.URL),
-  //     'kakaostorysharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=540');
-  //   return false;
-  // },
-  // 'click .fb-share': function () {
-  //   window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL),
-  //     'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=540,width=410');
-  //   return false;
-  // }
+
 })
