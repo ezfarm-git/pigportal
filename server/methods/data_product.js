f3eaaaa0ea6ee86df8ced406ee12c29412bd6d2d
wooyol <neolaocoon@naver.com>
2016-09-08 15:59:59 +0900

    edit some

--- server/methods/data_product.js
+++ server/methods/data_product.js
@@ -1,12 +1,12 @@
 Meteor.methods({
 
     "TWO_YEARS.get": function(pastY, curY) {
-        var response = HTTP.get('http://210.92.91.212:4100/pig-portal/two-years/' + pastY + '/' + curY);
+        var response = HTTP.get('http://localhost:4100/pig-portal/two-years/' + pastY + '/' + curY);
         return JSON.parse(response.content);
     },
 
     "CURRENT_WEEK.get": function() {
-        var response = HTTP.get('http://210.92.91.212:4100/pig-portal/currentweek');
+        var response = HTTP.get('http://localhost:4100/pig-portal/currentweek');
         return JSON.parse(response.content)[0]['WEEK'];
     }
 
