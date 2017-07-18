8dc61ad52abf5addafd91df0580636ebac1175ba
keon <rkftro246@naver.com>
2017-07-18 10:03:55 +0900

    modify main because of popup

--- client/templates/main/main.js
+++ client/templates/main/main.js
@@ -1,4 +1,4 @@
-// Template.main.onRendered(function() {
+ Template.main.onRendered(function() {
 //   $('.fancybox').fancybox({
 //       closeBtn: false,
 //       maxHeight: '600',
@@ -6,41 +6,47 @@
 //           title: {type: 'inside'}
 //       }
 //   });
-// })
 
-Template.main.helpers({
-    latestFocus: function() {
-        return Focus.find({}, {sort: {date: -1}, limit: 1});
-    },
-    // mainFocus: function() {
-    //     return Focus.find({}, {skip: 1, limit: 10}, {sort: {date: -1, title: 1}});
+    var popup = require('window-popup').windowPopup;
+    //window.open(500,500,'/news_popup.html');
+    popup(500,650,'/popup/popup');
+
+ //});
+
+//Template.main.helpers({
+
+    // latestFocus: function() {
+    //     return Focus.find({}, {sort: {date: -1}, limit: 1});
     // },
-    mainNews: function() {
-        return News.find({}, {sort: {date: -1, title: 1}, limit: 20});
-    },
-    mainCorps: function() {
-        return Corps.find({}, {sort: {date: -1, title: 1}, limit: 2});
-    },
-    mainCase: function() {
-        return Case.find({}, {sort: {date: -1}, limit: 2});
-    },
-    // mainCardNews: function() {
-    //     return CardNews.find({}, {sort: {date: -1}, limit: 1}).fetch()[0];
+    // // mainFocus: function() {
+    // //     return Focus.find({}, {skip: 1, limit: 10}, {sort: {date: -1, title: 1}});
+    // // },
+    // mainNews: function() {
+    //     return News.find({}, {sort: {date: -1, title: 1}, limit: 20});
     // },
-    // mainInfographic: function() {
-    //     return Infographic.find({}, {sort: {date: -1}, limit: 1}).fetch()[0];
+    // mainCorps: function() {
+    //     return Corps.find({}, {sort: {date: -1, title: 1}, limit: 2});
     // },
-    mainEvents: function() {
-        let recentEvents = Events.find({end: {$gte: moment().format('YYYY-MM-DD')}}, {sort: {start: 1}}).fetch();
-        $.each(recentEvents, function(key, value) {
-          if (value.start === value.end) {
-            value.end = null;
-          } else {
-            value.end = moment(value.end).add(-1, 'day').format('YYYY-MM-DD');
-          }
-        });
-        return recentEvents;
-    }
+    // mainCase: function() {
+    //     return Case.find({}, {sort: {date: -1}, limit: 2});
+    // },
+    // // mainCardNews: function() {
+    // //     return CardNews.find({}, {sort: {date: -1}, limit: 1}).fetch()[0];
+    // // },
+    // // mainInfographic: function() {
+    // //     return Infographic.find({}, {sort: {date: -1}, limit: 1}).fetch()[0];
+    // // },
+    // mainEvents: function() {
+    //     let recentEvents = Events.find({end: {$gte: moment().format('YYYY-MM-DD')}}, {sort: {start: 1}}).fetch();
+    //     $.each(recentEvents, function(key, value) {
+    //       if (value.start === value.end) {
+    //         value.end = null;
+    //       } else {
+    //         value.end = moment(value.end).add(-1, 'day').format('YYYY-MM-DD');
+    //       }
+    //     });
+    //     return recentEvents;
+    // }
 });
 
 // Template.main.events({
